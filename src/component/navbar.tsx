851e1ea6b9088e26f9e5d452e1980949e0ee2a57
waris <warisaremou2003@gmail.com>
2024-02-26 14:44:43 +0100

    Add motion animations to navbar

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -2,22 +2,68 @@ import { motion } from "framer-motion";
 import MobileNav from "./mobile-nav";
 
 export default function NavBar() {
+	const navBar = {
+		hide: {
+			y: -30,
+			opacity: 0,
+		},
+		show: {
+			y: 0,
+			opacity: 1,
+			transition: {
+				delayChildren: 1,
+				staggerChildren: 0.3,
+				delay: 0.2,
+				duration: 0.5,
+				ease: "easeInOut",
+			},
+		},
+	};
+
+	const link = {
+		hide: {
+			y: -30,
+			opacity: 0,
+		},
+		show: {
+			y: 0,
+			opacity: 1,
+			transition: {
+				duration: 0.5,
+			},
+		},
+	};
+
 	return (
-		<motion.div className="flex items-center justify-between">
+		<motion.div variants={navBar} initial="hide" animate="show" className="flex items-center justify-between">
 			<div>
 				<h1 className="text-2xl">Collecta</h1>
 			</div>
 
 			<ul className="text-gray-400 lg:flex items-center gap-14 [&>li:first-of-type]:text-gray-800 hidden">
-				<li className="relative before:content-[''] before:absolute before:-bottom-1 before:w-4 before:h-1 before:bg-gray-800 before:right-1/2 before:translate-x-1/2 before:rounded-full">
-					<a href="#">Home</a>
-				</li>
-				<li>
-					<a href="#">Community</a>
-				</li>
-				<li>
-					<a href="#">Collection</a>
-				</li>
+				<motion.li variants={link} className="nav-link">
+					<a href="#" className="">
+						Home
+					</a>
+				</motion.li>
+				<motion.li variants={link} className="nav-link group">
+					<a
+						href="#"
+						className=" flex flex-col relative [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-out overflow-hidden"
+					>
+						<span className="group-hover:-translate-y-6">Community</span>
+						<span className="absolute translate-y-6 group-hover:translate-y-0 text-gray-800">Community</span>
+					</a>
+				</motion.li>
+				<motion.li variants={link} className="nav-link group">
+					<a
+						href="#"
+						className=" flex flex-col relative [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-out overflow-hidden"
+					>
+						<span className="group-hover:-translate-y-6">Collection</span>
+						<span className="absolute translate-y-6 group-hover:translate-y-0 text-gray-800">Collection</span>
+					</a>
+				</motion.li>
 			</ul>
 			<div className="border border-[#e3e3e3] rounded-full p-3 max-lg:hidden">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
